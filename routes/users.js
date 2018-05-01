abe41a5c82892b7ae744a89286a5a2815951d678
Jahaziel <jahaziel.vaz@gmail.com>
2018-04-30 19:50:20 -0600

    New email duplicate verification at user route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 //Load User Model
 const User = require('../models/User')
@@ -16,11 +18,21 @@ router.post('/register', (req, res) => {
       if(user){
         return res.status(400).json({email: 'Email already exists'})
       } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: 200, //Size
+          r: 'pg' //Rating
+          d: 'mm' //Default
+        })
+
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
-          avatar: avatar,
+          avatar,
           password: req.body.password
+        })
+
+        bcrypt.genSalt(10, () = { //The first parameter is the number of characters, second is the callback. I believe genSalt, and salt are preset. Need to find out what they do.
+
         })
       }
     })
